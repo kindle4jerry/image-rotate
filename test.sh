ea3517698f87450ac12e66f60cdbbcaf2620177d
kindle4jerry <kindle4jerry@gmail.com>
2020-04-29 01:07:24 +0800

    Pick a example picture

--- test.sh
+++ test.sh
@@ -14,38 +14,11 @@ echo "Forward with anti-dogtooth"
 echo "Backward with anti-dogtooth"
 ./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
 ./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
-<<COMMENT
-echo "Forward with anti-dogtooth"
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 2
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 2
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 2
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 4
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 4
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 4
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 8
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 8
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 8
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 16
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 16
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 16
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 32
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 32
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45 32
-COMMENT
-echo "Backward with anti-dogtooth"
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 2
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 2
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 2
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 4
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 4
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 4
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 8
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 8
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 8
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 16
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 16
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 16
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 32
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 32
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45 32
+./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 4
+echo "OpenMP"
+for((i=1;i<=4;i++));  
+do   
+./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+done  
