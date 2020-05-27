9aa287dade20f03466f05c827e9148156ee908f0
kindle4jerry <kindle4jerry@gmail.com>
2020-05-28 01:14:48 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -4,21 +4,25 @@ INPUT_BMP=tank
 
 #COMMAND
 echo "No anti-dogtooth"
-./xc_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
-./xc_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
-./xc_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
+./xc_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
+./xc_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
+./xc_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-c.bmp 45
 echo "Forward with anti-dogtooth"
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
-./xcf_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
+./xcf_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
+./xcf_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
+./xcf_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cf.bmp 45
 echo "Backward with anti-dogtooth"
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 4
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-cb.bmp 45
 echo "OpenMP"
 for((i=1;i<=4;i++));  
 do   
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
-./xcb_omp_rorate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
-done  
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+./xcb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-omp.bmp 45 $i
+done
+echo "CUDA"
+./xgb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-gb.bmp 45
+./xgb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-gb.bmp 45
+./xgb_omp_rotate.x $INPUT_BMP.bmp $INPUT_BMP-45-gb.bmp 45
